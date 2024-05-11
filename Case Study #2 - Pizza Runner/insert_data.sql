ccde960422c9feb494de2b775bafd19cf0032cbb
wenyulll <wenyu414@gmail.com>
2024-05-11 12:04:21 -0700

    insert table

--- Case Study #2 - Pizza Runner/insert_data.sql
+++ Case Study #2 - Pizza Runner/insert_data.sql
@@ -3,3 +3,12 @@ CREATE TABLE runners (
     runner_id INT,
     registration_date DATE
 );
+-- Creating the customer_orders table
+CREATE TABLE customer_orders (
+    order_id INT,
+    customer_id INT,
+    pizza_id INT,
+    exclusions VARCHAR,
+    extras VARCHAR,
+    order_time TIMESTAMP
+);

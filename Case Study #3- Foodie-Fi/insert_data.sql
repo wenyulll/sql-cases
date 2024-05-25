17a74b0a4933ded2eb3e2742ab77766f9cc3f4cd
wenyulll <wenyu414@gmail.com>
2024-05-24 20:21:04 -0700

    insert data

--- Case Study #3- Foodie-Fi/insert_data.sql
+++ Case Study #3- Foodie-Fi/insert_data.sql
@@ -12,3 +12,11 @@ INSERT INTO foodie_fi.plans (plan_id, plan_name, price) VALUES
 (2, 'pro monthly', 19.90),
 (3, 'pro annual', 199),
 (4, 'churn', NULL);
+
+-- Create the subscriptions table
+CREATE TABLE foodie_fi.subscriptions (
+    customer_id INT,
+    plan_id INT,
+    start_date DATE,
+    PRIMARY KEY (customer_id, plan_id, start_date)
+);

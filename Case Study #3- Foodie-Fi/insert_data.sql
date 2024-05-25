58581b4e57dcbab1a732e0e3c16faca3b733f3a5
wenyulll <wenyu414@gmail.com>
2024-05-24 20:24:55 -0700

    insert readme

--- Case Study #3- Foodie-Fi/insert_data.sql
+++ Case Study #3- Foodie-Fi/insert_data.sql
@@ -43,3 +43,19 @@ INSERT INTO foodie_fi.subscriptions (customer_id, plan_id, start_date) VALUES
 (19, 0, '2020-06-22'),
 (19, 2, '2020-06-29'),
 (19, 3, '2020-08-29');
+
+-- 1. How many customers has Foodie-Fi ever had?
+
+SELECT * FROM 
+
+
+-- 2. What is the monthly distribution of trial plan start_date values for our dataset? Use the start of the month as the group by value.
+-- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name.
+-- 4. What is the customer count and percentage of customers who have churned, rounded to 1 decimal place?
+-- 5. How many customers have churned straight after their initial free trial? What percentage is this, rounded to the nearest whole number?
+-- 6. What is the number and percentage of customer plans after their initial free trial?
+-- 7. What is the customer count and percentage breakdown of all 5 plan_name values as of 2020-12-31?
+-- 8. How many customers have upgraded to an annual plan in 2020?
+-- 9. How many days on average does it take for a customer to upgrade to an annual plan from the day they join Foodie-Fi?
+-- 10. Can you further break down this average value into 30-day periods (i.e., 0-30 days, 31-60 days, etc.)?
+-- 11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?

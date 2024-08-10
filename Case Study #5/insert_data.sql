4532168911994c2326601cda420dee4e02994d49
wenyulll <wenyu414@gmail.com>
2024-08-09 23:24:03 -0700

    question 3

--- Case Study #5/insert_data.sql
+++ Case Study #5/insert_data.sql
@@ -54,3 +54,11 @@ SELECT DATE_TRUNC('month', event_time) AS month, COUNT(DISTINCT visit_id) AS uni
 FROM clique_bait.events
 GROUP BY month
 ORDER BY month;
+
+
+-- Events count by event type
+SELECT ei.event_name, COUNT(e.event_type) AS event_count
+FROM clique_bait.events e
+JOIN clique_bait.event_identifier ei
+ON e.event_type = ei.event_type
+GROUP BY ei.event_name;

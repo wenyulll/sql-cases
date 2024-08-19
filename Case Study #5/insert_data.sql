6c2897123261d72870a00183a938324289131f29
wenyulll <wenyu414@gmail.com>
2024-08-19 19:26:16 +0800

    question

--- Case Study #5/insert_data.sql
+++ Case Study #5/insert_data.sql
@@ -117,3 +117,14 @@ FROM clique_bait.events e
 JOIN clique_bait.page_hierarchy ph
 ON e.page_id = ph.page_id
 GROUP BY ph.page_name;
+-- Funnel data for each product category
+SELECT 
+    ph.product_category,
+    COUNT(CASE WHEN e.event_type = 1 THEN 1 END) AS views, 
+    COUNT(CASE WHEN e.event_type = 2 THEN 1 END) AS cart_adds, 
+    COUNT(CASE WHEN e.event_type = 2 AND e.visit_id NOT IN (SELECT visit_id FROM clique_bait.events WHERE event_type = 3) THEN 1 END) AS abandoned_cart,
+    COUNT(CASE WHEN e.event_type = 3 THEN 1 END) AS purchases
+FROM clique_bait.events e
+JOIN clique_bait.page_hierarchy ph
+ON e.page_id = ph.page_id
+GROUP BY ph.product_category;

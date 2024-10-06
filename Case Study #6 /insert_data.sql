acfbb108695c453f6ffeaefa0e84515185f40669
wenyulll <wenyu414@gmail.com>
2024-10-05 17:02:36 -0700

    insert data

--- Case Study #6 /insert_data.sql
+++ Case Study #6 /insert_data.sql
@@ -2,3 +2,11 @@ CREATE TABLE clique_bait.event_identifier (
   event_type INTEGER,
   event_name VARCHAR(50)
 );
+
+CREATE TABLE clique_bait.campaign_identifier (
+  campaign_id INTEGER,
+  products VARCHAR(10),
+  campaign_name VARCHAR(100),
+  start_date TIMESTAMP,
+  end_date TIMESTAMP
+);

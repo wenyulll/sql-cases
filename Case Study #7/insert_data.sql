a46d60d4fb47d7d9d477e775b00e3cde58e18acf
wenyulll <wenyu414@gmail.com>
2024-10-25 22:56:25 -0700

    create table

--- Case Study #7/insert_data.sql
+++ Case Study #7/insert_data.sql
@@ -1,1 +1,12 @@
-d
+CREATE TABLE balanced_tree.product_details (
+    product_id VARCHAR(10) PRIMARY KEY,
+    price DECIMAL(10, 2),
+    product_name VARCHAR(100),
+    category_id INT,
+    segment_id INT,
+    style_id INT,
+    category_name VARCHAR(50),
+    segment_name VARCHAR(50),
+    style_name VARCHAR(50)
+);
+

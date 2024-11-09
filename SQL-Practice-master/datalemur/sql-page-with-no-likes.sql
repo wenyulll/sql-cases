268b4ccf944e208791fda3b9bdcb5d42d2f5a691
wenyulll <wenyu414@gmail.com>
2024-11-08 20:02:25 -0800

    question

--- SQL-Practice-master/datalemur/sql-page-with-no-likes.sql
+++ SQL-Practice-master/datalemur/sql-page-with-no-likes.sql
@@ -11,3 +11,17 @@ WHERE
 ORDER BY 
   page_id
 ;
+
+SELECT
+  L.page_id
+FROM 
+  pages AS L 
+LEFT JOIN 
+  page_likes AS R 
+ON
+  L.page_id = R.page_id
+WHERE 
+  R.liked_date IS NULL
+ORDER BY 
+  page_id
+;

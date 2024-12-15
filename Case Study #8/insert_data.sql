6e432f6efe757aaeb229cb4f16e35a73143c463a
wenyulll <wenyu414@gmail.com>
2024-12-14 20:56:49 -0800

    create table

--- Case Study #8/insert_data.sql
+++ Case Study #8/insert_data.sql
@@ -1,1 +1,11 @@
-insert table 
+-- Create the interest_metrics table
+CREATE TABLE fresh_segments.interest_metrics (
+    _month INT,
+    _year INT,
+    month_year VARCHAR(7),
+    interest_id INT,
+    composition DECIMAL(5, 2),
+    index_value DECIMAL(5, 2),
+    ranking INT,
+    percentile_ranking DECIMAL(5, 2)
+);
